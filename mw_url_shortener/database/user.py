73960af94ed7ff8636244fe70135164e5ef63b51
Matthew Willcockson <matthew@willcockson.family>
2020-12-12 07:42:21 -0600

    refactor(database.user): add relevant errors and types

--- mw_url_shortener/database/user.py
+++ mw_url_shortener/database/user.py
@@ -2,9 +2,11 @@ print(f"imported mw_url_shortener.database.user as {__name__}")
 """
 This file exists purely to export an organized namespace
 """
+from .errors import UserAlreadyExistsError, UserNotFoundError
 from .interface import create_user as create
 from .interface import delete_user as delete
 from .interface import get_user as get
 from .interface import list_users as list
 from .interface import update_user as update
 from .models import UserModel as Model
+from ..types import HashedPassword, Username
