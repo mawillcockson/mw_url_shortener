29cc118aa25dc1b4cf8e967b1c2eee04508ebd70
Matthew Willcockson <matthew@willcockson.family>
2020-12-12 05:18:21 -0600

    style(database.user): isort and black
    applied both isort and black formatting tools

--- mw_url_shortener/database/user.py
+++ mw_url_shortener/database/user.py
@@ -2,5 +2,9 @@ print(f"imported mw_url_shortener.database.user as {__name__}")
 """
 This file exists purely to export an organized namespace
 """
+from .interface import create_user as create
+from .interface import delete_user as delete
+from .interface import get_user as get
+from .interface import list_users as list
+from .interface import update_user as update
 from .models import UserModel as Model
-from .interface import get_user as get, create_user as create, delete_user as delete, update_user as update, list_users as list
