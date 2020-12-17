28cd36cd59c3e2e93cfa1aea0eb6e229157c470a
Matthew Willcockson <matthew@willcockson.family>
2020-12-17 00:24:09 -0600

    fix(database.config): require proper settings classes
    ensures valid settings classes are used during saving and retrieval of configuration data

--- mw_url_shortener/database/config.py
+++ mw_url_shortener/database/config.py
@@ -1,8 +1,10 @@
 print(f"imported mw_url_shortener.database.config as {__name__}")
 from pony.orm import Database, db_session
+from pydantic import ValidationError
 
 from .. import settings
 from ..settings import CommonSettings, SettingsClasses, SettingsClassName
+from .errors import BadConfigInDBError
 
 
 def get_config(db: Database) -> CommonSettings:
@@ -17,14 +19,26 @@ def get_config(db: Database) -> CommonSettings:
         class_name = SettingsClassName.validate(current_config.class_name)
         settings_class = getattr(settings, class_name)
 
-    return settings_class.parse_raw(current_config_json)
+    try:
+        return settings_class.parse_raw(current_config_json)
+    except ValidationError as err:
+        raise BadConfigInDBError("bad configuration in database") from err
 
 
 def save_config(db: Database, new_settings: CommonSettings) -> CommonSettings:
     "encodes the settings and writes them to the database"
-    class_name = SettingsClassName.validate(new_settings.__class__.__name__)
+    bad_class_msg = "new_settings must be an instance of a settings class"
+    try:
+        class_name = SettingsClassName.validate(new_settings.__class__.__name__)
+    except ValidationError as err:
+        raise ValueError(bad_class_msg) from err
+
+    settings_class = getattr(settings, class_name)
+    if settings_class != type(new_settings):
+        raise ValueError(bad_class_msg)
+
     with db_session:
         config_entity = db.ConfigEntity(
             version="current", class_name=class_name, json=new_settings.json()
         )
-        return new_settings.__class__.parse_raw(config_entity.json)
+        return settings_class.parse_raw(config_entity.json)
