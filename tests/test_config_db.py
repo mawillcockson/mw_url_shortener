3c24547af33f570012124c53c0e370728e9463b1
Matthew Willcockson <matthew@willcockson.family>
2020-12-21 04:41:23 -0600

    test(config): add one database test
    Ensures the config.save_to_db() function works, and that config.get()
    can retrieve configuration information from the database.

---
+++ tests/test_config_db.py
@@ -0,0 +1,20 @@
+"""
+ensures the database portion of mw_url_shortener.config behaves correctly
+"""
+import pytest
+from pony.orm import Database
+from mw_url_shortener.settings import DatabaseSettings, CommonSettings
+from mw_url_shortener import settings, config
+
+
+def test_get_updates_from_db(database: Database, correct_database_settings: DatabaseSettings) -> None:
+    """
+    will config.get() update the module cache if the only place with settings
+    is the database
+    """
+    assert settings._settings is None
+    config.save_to_db(db=database, new_settings=correct_database_settings)
+
+    returned_settings = config.get()
+    assert returned_settings == correct_database_settings
+    assert settings._settings == correct_database_settings
