2717095e1162e9631471458120a9bd30c6ff6526
Matthew Willcockson <matthew@willcockson.family>
2021-12-10 10:10:24 -0600

    feat: add user schemas

---
+++ mw_url_shortener/schemas/user.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class User(UserInDBBase):
+    pass
+
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
