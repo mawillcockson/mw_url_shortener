1cceaeb3e32442951ad042390451faeeead3275e
Matthew Willcockson <matthew@willcockson.family>
2021-12-16 16:44:37 -0600

    refactor(database_interface/base): switch to AsyncSession
    previously was using sessionmaker[AsyncSession], hence the extra:
    async with async_session() as session:

--- mw_url_shortener/database_interface/base.py
+++ mw_url_shortener/database_interface/base.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
+# mypy: allow_any_expr
+from typing import Dict, Generic, List, Optional, Type, TypeVar, Union
 
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel as BaseSchema
@@ -19,40 +20,33 @@ class InterfaceBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         """
         self.model = model
 
-    async def get_by_id(
-        self, async_session: AsyncSession, id: Any
-    ) -> Optional[ModelType]:
-        async with async_session() as session:
-            user_model = await session.get(self.model, id)
-        return user_model
+    async def get_by_id(self, async_session: AsyncSession, id: int) -> ModelType:
+        object_model = (
+            await async_session.execute(select(self.model).where(self.model.id == id))
+        ).scalar_one()
+        assert isinstance(
+            object_model, self.model
+        ), f"expected '{type(self.model)}', got '{type(object_model)}'"
+        return object_model
 
     async def get_multiple(
         self, async_session: AsyncSession, *, skip: int = 0, limit: int = 100
     ) -> List[ModelType]:
-        async with async_session() as session:
-            async with session.begin():
-                user_models = (
-                    (
-                        await session.execute(
-                            select(self.model).offset(skip).limit(limit)
-                        )
-                    )
-                    .scalars()
-                    .all()
-                )
-            for user_model in user_models:
-                await session.refresh(user_model)
-            return user_models
+        async with async_session.begin():
+            statement = select(self.model).offset(skip).limit(limit)
+            object_models = (await async_session.execute(statement)).scalars().all()
+        for object_model in object_models:
+            await async_session.refresh(object_model)
+        return object_models
 
     async def create(
         self, async_session: AsyncSession, *, object_schema_in: CreateSchemaType
     ) -> ModelType:
         object_in_data = jsonable_encoder(object_schema_in)
-        object_model = self.model(**object_in_data)  # type: ignore
-        async with async_session() as session:
-            async with session.begin():
-                session.add(object_model)
-            await session.refresh(object_model)
+        object_model = self.model(**object_in_data)
+        async with async_session.begin():
+            async_session.add(object_model)
+        await async_session.refresh(object_model)
         return object_model
 
     async def update(
@@ -63,23 +57,18 @@ class InterfaceBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         object_update_schema: UpdateSchemaType,
     ) -> ModelType:
         current_data = jsonable_encoder(current_object_model)
-        if isinstance(object_update_schema, dict):
-            update_data = object_update_schema
-        else:
-            update_data = object_update_schema.dict(exclude_unset=True)
+        update_data = object_update_schema.dict(exclude_unset=True)
         for field in current_data:
             if field in update_data:
                 setattr(current_object_model, field, update_data[field])
 
-        async with async_session() as session:
-            async with session.begin():
-                session.add(current_object_model)
-            await session.refresh(current_object_model)
+        async with async_session.begin():
+            async_session.add(current_object_model)
+        await async_session.refresh(current_object_model)
         return current_object_model
 
     async def remove_by_id(self, async_session: AsyncSession, *, id: int) -> ModelType:
         object_model = await self.get_by_id(async_session, id=id)
-        async with async_session() as session:
-            async with session.begin():
-                await session.delete(object_model)
+        async with async_session.begin():
+            await async_session.delete(object_model)
         return object_model
