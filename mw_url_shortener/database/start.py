3be95174cf194cbb216066860a01abdb4e407dac
Matthew Willcockson <matthew@willcockson.family>
2021-12-16 15:39:27 -0600

    chore(database/start): correct types
    Only the AsyncEngine has a .dispose() method, which is used to close all
    connections in a pool. This is handy for using with multiple event
    loops:
    https://docs.sqlalchemy.org/en/20/orm/extensions/asyncio.html#using-multiple-asyncio-event-loops
    
    I don't think I'll be doing that: even at the terminal, with Typer, the
    app can be organized to only use one event loop.

--- mw_url_shortener/database/start.py
+++ mw_url_shortener/database/start.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .models.registry import mapper_registry
 
 
-async def make_session(database_url: str) -> AsyncSession:
+async def make_session(database_url: str) -> "sessionmaker[AsyncSession]":
     "creates the main way to talk to the database"
     engine = create_async_engine(database_url, echo=True, future=True)
 
@@ -13,16 +13,7 @@ async def make_session(database_url: str) -> AsyncSession:
     # here to "initialize" a database file
 
     async with engine.begin() as connection:
-        await connection.run_sync(mapper_registry.metadata.create_all)
+        await connection.run_sync(mapper_registry.metadata.create_all)  # type: ignore
 
-    return sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)
-
-
-async def close_session(session: AsyncSession) -> None:
-    """
-    a reminder that, if AsyncSession isn't used inside an async with
-    statement, this method should be called to perform cleanup
-
-    hopefully doesn't have to be used
-    """
-    await session.dispose()
+    async_sessionmaker = sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)  # type: ignore
+    return async_sessionmaker
