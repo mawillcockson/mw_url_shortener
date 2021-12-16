573e4bbb2f11b574f7723b378241fb3555f50cfe
Matthew Willcockson <matthew@willcockson.family>
2021-12-16 14:52:39 -0600

    refactor(security): satisfy mypy

--- mw_url_shortener/security.py
+++ mw_url_shortener/security.py
@@ -1,14 +1,22 @@
 """
 all of the authentication and authorization helper functions
 """
+from typing import cast
+
 from passlib.context import CryptContext
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")  # type: ignore
 
 
 def hash_password(password: str) -> str:
-    return password_context.hash(password)
+    hashed_password = cast(str, password_context.hash(password))  # type: ignore
+    if not isinstance(hashed_password, str):
+        raise TypeError(f"expected str, instead got '{type(hashed_password)}'")
+    return hashed_password
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return password_context.verify(plain_password, hashed_password)
+    is_valid = cast(bool, password_context.verify(plain_password, hashed_password))  # type: ignore
+    if not isinstance(is_valid, bool):
+        raise TypeError(f"expected bool, instead got '{type(is_valid)}'")
+    return bool(is_valid)
