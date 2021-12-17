5a3d64d52ef0b8b5ac441d328e08095830871730
Matthew Willcockson <matthew@willcockson.family>
2021-12-17 11:33:30 -0600

    refactor(settings): add explicit types

--- mw_url_shortener/settings.py
+++ mw_url_shortener/settings.py
@@ -5,23 +5,23 @@ from pathlib import Path
 from typing import Optional
 
 import platformdirs
-from pydantic import BaseSettings
+from pydantic import BaseSettings, PositiveInt
 
 from . import APP_AUTHOR, APP_NAME, __version__
 
 
 class Defaults(BaseSettings):
     # NOTE:FEAT redirects.sqlite should be in a more "typical" location
-    config_path = Path(
+    config_path: Path = Path(
         platformdirs.user_config_dir(
             appname=APP_NAME, appauthor=APP_AUTHOR, version=__version__, roaming=True
         )
     )
-    database_path = Path("/var/db/redirects.sqlite")
-    database_url_scheme = "sqlite+aiosqlite"
-    database_url = f"{database_url_scheme}:///{database_path}"
-    max_username_length = 30
-    max_password_length = 128
+    database_path: Path = Path("/var/db/redirects.sqlite")
+    database_url_scheme: str = "sqlite+aiosqlite"
+    database_url: str = f"{database_url_scheme}:///{database_path}"
+    max_username_length: PositiveInt = 30
+    max_password_length: PositiveInt = 128
 
     class Config:
         allow_mutation = False
