990d025decd32584a2543b82ffaa15d8f936f253
Matthew Willcockson <matthew@willcockson.family>
2021-12-17 01:06:19 -0600

    refactor: explicitly ignore extra attributes on base schema

--- mw_url_shortener/schemas/base.py
+++ mw_url_shortener/schemas/base.py
@@ -1,10 +1,11 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Extra
 
 
 class BaseSchema(BaseModel):
-    pass
+    class Config:
+        extra = Extra.ignore
 
 
 class BaseInDBSchema(BaseSchema):
