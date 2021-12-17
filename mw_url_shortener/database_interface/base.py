9b45856231254e536f6dc3187f76e814286362e1
Matthew Willcockson <matthew@willcockson.family>
2021-12-16 20:22:05 -0600

    feat: return only schemas from interface

--- mw_url_shortener/database_interface/base.py
+++ mw_url_shortener/database_interface/base.py
@@ -9,45 +9,49 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from mw_url_shortener.database.models.base import DeclarativeBase
 
 ModelType = TypeVar("ModelType", bound=DeclarativeBase)
+ObjectSchemaType = TypeVar("ObjectSchemaType", bound=BaseSchema)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseSchema)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseSchema)
 
 
-class InterfaceBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: Type[ModelType]):
+class InterfaceBase(
+    Generic[ModelType, ObjectSchemaType, CreateSchemaType, UpdateSchemaType]
+):
+    def __init__(self, model: Type[ModelType], schema: Type[ObjectSchemaType]):
         """
         interface object with default methods to create, read, update, delete
         """
         self.model = model
+        self.schema = schema
 
-    async def get_by_id(self, async_session: AsyncSession, id: int) -> ModelType:
+    async def get_by_id(self, async_session: AsyncSession, id: int) -> ObjectSchemaType:
         object_model = (
             await async_session.execute(select(self.model).where(self.model.id == id))
         ).scalar_one()
         assert isinstance(
             object_model, self.model
         ), f"expected '{self.model}', got '{type(object_model)}'"
-        return object_model
+        return self.schema.from_orm(object_model)
 
     async def get_multiple(
         self, async_session: AsyncSession, *, skip: int = 0, limit: int = 100
-    ) -> List[ModelType]:
+    ) -> List[ObjectSchemaType]:
+        objects: List[ObjectSchemaType] = []
         async with async_session.begin():
             statement = select(self.model).offset(skip).limit(limit)
             object_models = (await async_session.execute(statement)).scalars().all()
-        for object_model in object_models:
-            await async_session.refresh(object_model)
-        return object_models
+            for object_model in object_models:
+                objects.append(self.schema.from_orm(object_models))
+        return objects
 
     async def create(
         self, async_session: AsyncSession, *, object_schema_in: CreateSchemaType
-    ) -> ModelType:
+    ) -> ObjectSchemaType:
         object_in_data = jsonable_encoder(object_schema_in)
         object_model = self.model(**object_in_data)
         async with async_session.begin():
             async_session.add(object_model)
-        await async_session.refresh(object_model)
-        return object_model
+            return self.schema.from_orm(object_model)
 
     async def update(
         self,
@@ -55,7 +59,7 @@ class InterfaceBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         *,
         current_object_model: ModelType,
         object_update_schema: UpdateSchemaType,
-    ) -> ModelType:
+    ) -> ObjectSchemaType:
         current_data = jsonable_encoder(current_object_model)
         update_data = object_update_schema.dict(exclude_unset=True)
         for field in current_data:
@@ -64,11 +68,12 @@ class InterfaceBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
 
         async with async_session.begin():
             async_session.add(current_object_model)
-        await async_session.refresh(current_object_model)
-        return current_object_model
+            return self.schema.from_orm(current_object_model)
 
-    async def remove_by_id(self, async_session: AsyncSession, *, id: int) -> ModelType:
+    async def remove_by_id(
+        self, async_session: AsyncSession, *, id: int
+    ) -> ObjectSchemaType:
         object_model = await self.get_by_id(async_session, id=id)
         async with async_session.begin():
             await async_session.delete(object_model)
-        return object_model
+            return self.schema.from_orm(object_model)
