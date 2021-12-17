18f153af5836cf375b63c3f31067c112b54d3803
Matthew Willcockson <matthew@willcockson.family>
2021-12-17 01:01:07 -0600

    fix: make id optional on base schema

--- mw_url_shortener/schemas/base.py
+++ mw_url_shortener/schemas/base.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -6,4 +8,4 @@ class BaseSchema(BaseModel):
 
 
 class BaseInDBSchema(BaseSchema):
-    id: int
+    id: Optional[int] = None
