a37e910501a3d1926b818cc421b936a5d9ed132b
Matthew Willcockson <matthew@willcockson.family>
2021-12-17 18:36:12 -0600

    test: import needed defaults

--- tests/database/test_redirect.py
+++ tests/database/test_redirect.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from mw_url_shortener import database_interface
 from mw_url_shortener.schemas.redirect import RedirectCreate, RedirectUpdate
 from mw_url_shortener.security import verify_password
+from mw_url_shortener.settings import defaults
 from tests.utils import random_password, random_username
 
 
@@ -66,8 +67,8 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #     assert hasattr(created_user, "id")
 #     assert created_user.id is not None
 #     assert not hasattr(created_user, "hashed_password")
-# 
-# 
+#
+#
 # async def test_authenticate_user(in_memory_database: AsyncSession) -> None:
 #     "can an added user's data be used to authenticate the user?"
 #     username = random_username()
@@ -81,8 +82,8 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #     )
 #     assert user_authenticated
 #     assert user_authenticated == user_created
-# 
-# 
+#
+#
 # async def test_not_authenticate_user(in_memory_database: AsyncSession) -> None:
 #     "will authentication fail for user data not in the database?"
 #     username = random_username()
@@ -91,8 +92,8 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #         in_memory_database, username=username, password=password
 #     )
 #     assert user_created is None
-# 
-# 
+#
+#
 # async def test_get_user_by_id(in_memory_database: AsyncSession) -> None:
 #     "can a previously added user be retrieved by id, and is the data the same?"
 #     username = random_username()
@@ -106,8 +107,8 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #     )
 #     assert user_retrieved
 #     assert user_created == user_retrieved
-# 
-# 
+#
+#
 # async def test_get_two_users(in_memory_database: AsyncSession) -> None:
 #     "can a previous two users be retrieved simultaneously?"
 #     username1 = random_username()
@@ -116,22 +117,22 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #     user_created1 = await database_interface.user.create(
 #         in_memory_database, create_object_schema=user_create_schema1
 #     )
-# 
+#
 #     username2 = random_username()
 #     password2 = random_password()
 #     user_create_schema2 = UserCreate(username=username2, password=password2)
 #     user_created2 = await database_interface.user.create(
 #         in_memory_database, create_object_schema=user_create_schema2
 #     )
-# 
+#
 #     retrieved_users = await database_interface.user.get_multiple(
 #         in_memory_database, skip=0, limit=100
 #     )
 #     assert len(retrieved_users) == 2
 #     assert user_created1 in retrieved_users
 #     assert user_created2 in retrieved_users
-# 
-# 
+#
+#
 # async def test_update_user_password(in_memory_database: AsyncSession) -> None:
 #     """
 #     if a user password is modified, does the database return the modified user?
@@ -142,37 +143,37 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #     user_created = await database_interface.user.create(
 #         in_memory_database, create_object_schema=user_create_schema
 #     )
-# 
+#
 #     new_password = random_password()
 #     user_update_schema = UserUpdate(password=new_password)
-# 
+#
 #     await database_interface.user.update(
 #         in_memory_database,
 #         current_object_schema=user_created,
 #         update_object_schema=user_update_schema,
 #     )
-# 
+#
 #     user_retrieved = await database_interface.user.get_by_id(
 #         in_memory_database, id=user_created.id
 #     )
-# 
+#
 #     assert user_retrieved
 #     assert user_created == user_retrieved
-# 
+#
 #     # affirm authentication fails if the old password is used
 #     authentication_with_old_password = await database_interface.user.authenticate(
 #         in_memory_database, username=user_retrieved.username, password=password
 #     )
 #     assert not authentication_with_old_password
-# 
+#
 #     # affirm authentication succeeds when new password is used
 #     authenticated_user = await database_interface.user.authenticate(
 #         in_memory_database, username=user_retrieved.username, password=new_password
 #     )
 #     assert authenticated_user
 #     assert authenticated_user == user_created
-# 
-# 
+#
+#
 # async def test_delete_user(in_memory_database: AsyncSession) -> None:
 #     "if a user is deleted, can their data no longer be found in the database?"
 #     # create user
@@ -183,7 +184,7 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #         in_memory_database,
 #         create_object_schema=user_create_schema,
 #     )
-# 
+#
 #     # affirm user is in database
 #     user_retrieved = await database_interface.user.get_by_id(
 #         in_memory_database, user_created.id
@@ -191,7 +192,7 @@ async def test_create_redirect_defaults(in_memory_database: AsyncSession) -> Non
 #     # roundtripping with get_by_id() is tested more thoroughly elsewhere, no
 #     # need to test it again here
 #     assert user_retrieved
-# 
+#
 #     deleted_user = await database_interface.user.remove_by_id(
 #         in_memory_database, id=user_retrieved.id
 #     )
