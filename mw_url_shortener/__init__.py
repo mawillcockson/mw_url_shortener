93c0e20b842f091258be6d676f885b39610be913
Matthew Willcockson <matthew@willcockson.family>
2021-12-22 20:33:33 -0600

    refactor: import entire importlib.metadata module

--- mw_url_shortener/__init__.py
+++ mw_url_shortener/__init__.py
@@ -3,11 +3,11 @@ import sys
 # From:
 # https://github.com/mawillcockson/eggord/blob/ea7e56ce173561a550a08b67a9dafdaec149ff17/eggord/__init__.py
 if sys.version_info >= (3, 8):
-    from importlib.metadata import metadata as package_metadata
+    from importlib import metadata as package_metadata
 else:
-    from importlib_metadata import metadata as package_metadata
+    import importlib_metadata as package_metadata
 
 APP_NAME = __name__
-metadata = package_metadata(APP_NAME)
+metadata = package_metadata.metadata(APP_NAME)
 __version__ = str(metadata["version"])
 APP_AUTHOR = str(metadata["author"])
