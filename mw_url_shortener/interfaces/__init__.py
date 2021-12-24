41a94a257f84ffec2c6f3813f1aeca7e6b1fc40b
Matthew Willcockson <matthew@willcockson.family>
2021-12-24 17:21:12 -0600

    refactor(interfaces): explicitly reexport data-specific interfaces

--- mw_url_shortener/interfaces/__init__.py
+++ mw_url_shortener/interfaces/__init__.py
@@ -8,5 +8,5 @@ from .helpers import inject_interface as inject_interface
 from .helpers import inject_resource as inject_resource
 from .helpers import open_resource as open_resource
 from .helpers import run_sync as run_sync
-from .redirect_interface import RedirectInterface
-from .user_interface import UserInterface
+from .redirect_interface import RedirectInterface as RedirectInterface
+from .user_interface import UserInterface as UserInterface
