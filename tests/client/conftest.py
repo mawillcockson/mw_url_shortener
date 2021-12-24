e5400201cb8841907144cc7dcdd4df3cb21dc661
Matthew Willcockson <matthew@willcockson.family>
2021-12-24 17:37:01 -0600

    test(client): ensure dependency injection is cleared, and an async event loop is running

--- tests/client/conftest.py
+++ tests/client/conftest.py
@@ -1,7 +1,14 @@
+from typing import AsyncIterator
+
 import pytest
 from typer.testing import CliRunner
 
+from mw_url_shortener.dependency_injection import initialize_depency_injection
+
 
 @pytest.fixture
-def cli_test_client(anyio_backend: str) -> CliRunner:
-    return CliRunner()
+async def cli_test_client(anyio_backend: str) -> AsyncIterator[CliRunner]:
+    inject.clear()
+    initialize_depency_injection()
+    yield CliRunner()
+    inject.clear()
