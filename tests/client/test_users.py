d0a8ec8c813e9f38cc271098021dbb3fcd1b8bd6
Matthew Willcockson <matthew@willcockson.family>
2021-12-24 19:12:42 -0600

    test(client): first attempt at running tests

--- tests/client/test_users.py
+++ tests/client/test_users.py
@@ -1,6 +1,7 @@
 """
 do all cli commands dealing with users work correctly?
 """
+import asyncio
 from pathlib import Path
 
 from sqlalchemy import and_, select
@@ -8,33 +9,34 @@ from typer.testing import CliRunner
 
 from mw_url_shortener.cli.entry_point import app
 
-from .utils import random_password, random_username
+from ..utils import random_password, random_username
 
-runner = CliRunner()
 
-# NOTE:BUG isort can't handle the below syntax
+async def test_create_user(on_disk_database: Path, cli_test_client: CliRunner) -> None:
+    "can a user be created and read back?"
+    test_username = random_username()
+    test_password = random_password()
 
-# async def test_create_user(on_disk_database: Path) -> None:
-#     "can a user be created and read back?"
-#     test_username = random_username()
-#     test_password = random_password()
-#
-#     result = runner.invoke(
-#         app,
-#         [
-#             "--local",
-#             str(on_disk_database),
-#             "user",
-#             "create",
-#             "--username",
-#             test_username,
-#             "--password",
-#             test_password,
-#         ],
-#     )
-#     assert result.exit_code == 0
-#     assert (
-#         f"user '{test_username}' created with id '{expected_user_id}'" in result.stdout
-#     )
-#
-#     retrieved_user = await database_interface.user.get_by_id()
+    result = await asyncio.get_running_loop().run_in_executor(
+        None,
+        cli_test_client.invoke,
+        app,
+        [
+            "--output-style=json",
+            "local",
+            "--database-path",
+            str(on_disk_database),
+            "user",
+            "create",
+            "--username",
+            test_username,
+            "--password",
+            test_password,
+        ],
+    )
+    assert result.exit_code == 0
+    assert (
+        f"user '{test_username}' created with id '{expected_user_id}'" in result.stdout
+    )
+
+    retrieved_user = await database_interface.user.get_by_id()
