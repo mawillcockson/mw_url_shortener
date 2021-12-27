7a2495f74a24d1b626693056947476bc8230037f
Matthew Willcockson <matthew@willcockson.family>
2021-12-27 03:11:53 -0600

    test(client): check that database was used

--- tests/client/test_users.py
+++ tests/client/test_users.py
@@ -5,11 +5,16 @@ from functools import partial
 from pathlib import Path
 from typing import Callable
 
+import inject
 from click.testing import Result
 from typer.testing import CliRunner
 
 from mw_url_shortener.cli.entry_point import app
+from mw_url_shortener.database.start import AsyncSession, make_sessionmaker
+from mw_url_shortener.interfaces import Resource
+from mw_url_shortener.interfaces import database as database_interface
 from mw_url_shortener.schemas.user import User
+from mw_url_shortener.settings import Settings
 
 from ..utils import random_password, random_username
 from .conftest import Result, TestCommand
@@ -41,11 +46,20 @@ async def test_create_user(
         ],
     )
 
-    initial_database_size = on_disk_database.stat().st_size
-
     result = await run_test_command(test_command)
-    assert result.exit_code == 0
+    assert result.exit_code == 0, f"result: {result}"
     created_user = User.parse_raw(result.stdout)
     assert created_user
-    # assert the database was used?
-    assert on_disk_database.stat().st_size > initial_database_size
+
+    # I think this uses too many of the implementation details, but I also
+    # think it's maybe okay to do, to confirm that the correct database is
+    # being used
+    settings = inject.instance(Settings)
+    database_url_end = settings.database_url_joiner + str(on_disk_database)
+    assert settings.database_url.endswith(database_url_end)
+    async_sessionmaker = await make_sessionmaker(settings.database_url)
+    async with async_sessionmaker() as async_session:
+        retrieved_user = await database_interface.user.get_by_id(
+            async_session, id=created_user.id
+        )
+    assert retrieved_user == created_user
