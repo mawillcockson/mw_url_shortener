3ec23d16b679f18c0790263e3532ad5f8b3cb2bd
Matthew Willcockson <matthew@willcockson.family>
2021-12-27 00:58:22 -0600

    test(client): simplify test invocation

--- tests/client/conftest.py
+++ tests/client/conftest.py
@@ -1,8 +1,13 @@
-from typing import Iterator
+import asyncio
+from typing import Awaitable, Callable, Iterator
 
 import inject
 import pytest
+from click.testing import Result as Result
+from pytest import CaptureFixture
 from typer.testing import CliRunner
+
+from mw_url_shortener.dependency_injection import initialize_depency_injection
 
 
 @pytest.fixture
@@ -10,3 +15,19 @@ def cli_test_client() -> Iterator[CliRunner]:
     inject.clear()
     yield CliRunner()
     inject.clear()
+
+
+TestCommand = Callable[[], Result]
+
+
+@pytest.fixture
+def run_test_command(
+    capsys: CaptureFixture[str],
+) -> Callable[[TestCommand], Awaitable[Result]]:
+    async def runner(test_command: TestCommand) -> Result:
+        initialize_depency_injection()
+
+        with capsys.disabled():
+            return await asyncio.get_running_loop().run_in_executor(None, test_command)
+
+    return runner
--- tests/client/test_users.py
+++ tests/client/test_users.py
@@ -1,49 +1,49 @@
 """
 do all cli commands dealing with users work correctly?
 """
-import asyncio
 from functools import partial
 from pathlib import Path
+from typing import Callable
 
 from click.testing import Result
-from pytest import CaptureFixture
-from sqlalchemy import and_, select
 from typer.testing import CliRunner
 
 from mw_url_shortener.cli.entry_point import app
-from mw_url_shortener.dependency_injection import initialize_depency_injection
 from mw_url_shortener.schemas.user import User
 
 from ..utils import random_password, random_username
+from .conftest import Result, TestCommand
 
 
-async def test_create_user(on_disk_database: Path, capsys: CaptureFixture) -> None:
+async def test_create_user(
+    on_disk_database: Path,
+    run_test_command: Callable[[TestCommand], Result],
+    cli_test_client: CliRunner,
+) -> None:
     "can a user be created and read back?"
     test_username = random_username()
     test_password = random_password()
 
-    def runner() -> Result:
-        cli_test_client = CliRunner()
+    test_command = partial(
+        cli_test_client.invoke,
+        app,
+        [
+            "--output-style=json",
+            "local",
+            "--database-path",
+            str(on_disk_database),
+            "user",
+            "create",
+            "--username",
+            test_username,
+            "--password",
+            test_password,
+        ],
+    )
 
-        return cli_test_client.invoke(
-            app,
-            [
-                "--output-style=json",
-                "local",
-                "--database-path",
-                str(on_disk_database),
-                "user",
-                "create",
-                "--username",
-                test_username,
-                "--password",
-                test_password,
-            ],
-        )
+    assert on_disk_database.exists()
 
-    initialize_depency_injection()
-    with capsys.disabled():
-        result = await asyncio.get_running_loop().run_in_executor(None, runner)
+    result = await run_test_command(test_command)
     assert result.exit_code == 0
     created_user = User.parse_raw(result.stdout)
     assert created_user
