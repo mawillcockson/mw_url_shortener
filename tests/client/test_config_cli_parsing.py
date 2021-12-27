71a3378f995444c9d7373e470cfc965781303e03
Matthew Willcockson <matthew@willcockson.family>
2021-12-27 03:22:38 -0600

    test(client): test maybe more than necessary

--- tests/client/test_config_cli_parsing.py
+++ tests/client/test_config_cli_parsing.py
@@ -1,25 +1,27 @@
 """
 can the app take all of it's configuration through command line parameters?
 """
+from functools import partial
 from pathlib import Path
+from typing import Callable
 
+from click.testing import Result
 from typer.testing import CliRunner
 
 from mw_url_shortener.cli.entry_point import OutputStyle, app
-from mw_url_shortener.settings import Settings
-from typing import Callable
-from click.testing import Result
-from functools import partial
+from mw_url_shortener.settings import Settings, defaults
 
 
-async def test_database_path(tmp_path: Path,
+async def test_database_path(
+    tmp_path: Path,
     run_test_command: Callable[[TestCommand], Result],
     cli_test_client: CliRunner,
-        ) -> None:
+) -> None:
     "is the database path accepted as a command-line parameter?"
     database_path = tmp_path / "temporary_database.sqlite"
 
-    test_command = partial(cli_test_client.invoke,
+    test_command = partial(
+        cli_test_client.invoke,
         app,
         [
             "--output-style",
@@ -34,5 +36,9 @@ async def test_database_path(tmp_path: Path,
     result = await run_test_command(test_command)
     assert result.exit_code == 0
 
+    expected_settings = defaults.copy(update={"database_path": str(database_path)})
     settings = Settings.parse_raw(result.stdout)
-    assert settings.database_path == database_path
+    assert settings == expected_settings
+
+    # make sure no local files were modified
+    assert not database_path.exists()
