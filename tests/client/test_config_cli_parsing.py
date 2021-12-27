926b910cc0b34c1e9b58edee9b91e2dbf4ab706a
Matthew Willcockson <matthew@willcockson.family>
2021-12-27 03:21:45 -0600

    test(client): use new invocation in cli parsing

--- tests/client/test_config_cli_parsing.py
+++ tests/client/test_config_cli_parsing.py
@@ -5,24 +5,33 @@ from pathlib import Path
 
 from typer.testing import CliRunner
 
-from mw_url_shortener.cli.entry_point import Style, app
+from mw_url_shortener.cli.entry_point import OutputStyle, app
 from mw_url_shortener.settings import Settings
+from typing import Callable
+from click.testing import Result
+from functools import partial
 
 
-def test_database_path(tmp_path: Path, cli_test_client: CliRunner) -> None:
+async def test_database_path(tmp_path: Path,
+    run_test_command: Callable[[TestCommand], Result],
+    cli_test_client: CliRunner,
+        ) -> None:
     "is the database path accepted as a command-line parameter?"
     database_path = tmp_path / "temporary_database.sqlite"
 
-    result = cli_test_client.invoke(
+    test_command = partial(cli_test_client.invoke,
         app,
         [
+            "--output-style",
+            OutputStyle.json.value,
+            "local",
             "--database-path",
             str(database_path),
             "show-configuration",
-            "--style",
-            Style.json.value,
         ],
     )
+
+    result = await run_test_command(test_command)
     assert result.exit_code == 0
 
     settings = Settings.parse_raw(result.stdout)
