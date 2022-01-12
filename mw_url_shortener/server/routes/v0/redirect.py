9cc5b9487ba45674bc24c0c65d444c37c354340c
Matthew Willcockson <matthew@willcockson.family>
2022-01-12 16:38:46 -0600

    fix(api/v0/redirect): use response_status and body

--- mw_url_shortener/server/routes/v0/redirect.py
+++ mw_url_shortener/server/routes/v0/redirect.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 
 from fastapi import APIRouter, Depends, HTTPException, Request
-from starlette.responses import RedirectResponse
+from starlette.responses import Response
 
 from mw_url_shortener.database.start import AsyncSession
 from mw_url_shortener.interfaces.database import redirect as redirect_interface
@@ -13,7 +13,7 @@ async def match_redirect(
     request: Request,
     short_link: str,
     async_session: AsyncSession = Depends(get_async_session),
-) -> RedirectResponse:
+) -> Response:
     """
     return a redirect that matches the short link
 
@@ -22,10 +22,19 @@ async def match_redirect(
     redirect_schemas = await redirect_interface.search(
         async_session, short_link=short_link
     )
-    if len(redirect_schemas) == 1:
-        return RedirectResponse(url=str(redirect_schemas[0].url))
+    if len(redirect_schemas) != 1:
+        raise HTTPException(status_code=404, detail="redirect not found")
 
-    raise HTTPException(status_code=404, detail="redirect not found")
+    redirect_schema = redirect_schemas[0]
+    headers = {
+        "Location": str(redirect_schema.url),
+    }
+    return Response(
+        headers=headers,
+        status_code=int(redirect_schema.response_status),
+        content=str(redirect_schema.body),
+        media_type="text/plain",
+    )
 
 
 router = APIRouter()
