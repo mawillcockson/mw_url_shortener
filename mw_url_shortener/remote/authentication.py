dbd3a20090aa20303a15d3b3c2f6608d284b8e0c
Matthew Willcockson <matthew@willcockson.family>
2022-01-15 15:58:33 -0600

    fix(remote/authentication): handle error cases better
    if token is presented first, the original method wouldn't refresh it if it's expired

--- mw_url_shortener/remote/authentication.py
+++ mw_url_shortener/remote/authentication.py
@@ -22,6 +22,8 @@ from mw_url_shortener.schemas.security import (
 from mw_url_shortener.schemas.user import Password, Username
 
 if TYPE_CHECKING:
+    from typing import Dict
+
     from mw_url_shortener.settings import Settings
 
 auth_lock = asyncio.Lock()
@@ -65,68 +67,76 @@ class OAuth2PasswordBearerHandler(Auth):
     async def async_auth_flow(
         self, request: Request
     ) -> AsyncGenerator[Request, Response]:
+        # NOTE:BUG::AUTHENTICATION this could potentially expose the token,
+        # username, and password to a mistyped base_url
+        if self.token is not None:
+            # NOTE:TYPES httpx.Request lists the type of headers as requiring a
+            # Dict, when it'd be fine with a Mapping
+            # request.headers.update(authorization_headers)
+            request.headers["Authorization"] = self.authorization_headers[
+                "Authorization"
+            ]
+
         response = yield request
 
         if not self.need_authentication(response):
             return
 
-        if self.token is None:
-            # refresh token
-
-            if not (self.username and self.password):
-                raise HTTPStatusError(
-                    message="cannot refresh token without username and password",
-                    request=response.request,
-                    response=response,
-                )
-
-            form_data = OAuth2PasswordRequestFormData(
-                username=self.username,
-                password=self.password,
+        # refresh token
+        if not (self.username and self.password):
+            raise HTTPStatusError(
+                message="cannot refresh token without username and password",
+                request=response.request,
+                response=response,
             )
 
-            # the authentication lock should be held until self.token is written
-            # to, to prevent another call from checking if self.token is None, and
-            # starting another refresh_token() that will wait at the auth_lock
-            # until this one completes
-            async with auth_lock:
-                token_response = yield Request(
-                    method="POST",
-                    url=self.oauth2_endpoint_url,
-                    data=cast("Dict[str, str]", form_data),
+        form_data = OAuth2PasswordRequestFormData(
+            username=self.username,
+            password=self.password,
+        )
+
+        # the authentication lock should be held until self.token is written
+        # to, to prevent another call from checking if self.token is None, and
+        # starting another refresh_token() that will wait at the auth_lock
+        # until this one completes
+        async with auth_lock:
+            token_response = yield Request(
+                method="POST",
+                url=self.oauth2_endpoint_url,
+                data=cast("Dict[str, str]", form_data),
+            )
+
+            status_code = token_response.status_code
+            assert isinstance(
+                status_code, int
+            ), f"expected status_code to be int, not '{type(status_code)}': '{status_code}'"
+            if status_code != 200:
+                raise HTTPStatusError(
+                    message="cannot refresh token",
+                    request=token_response.request,
+                    response=token_response,
                 )
 
-                status_code = token_response.status_code
-                assert isinstance(
-                    status_code, int
-                ), f"expected status_code to be int, not '{type(status_code)}': '{status_code}'"
-                if status_code != 200:
-                    raise HTTPStatusError(
-                        message="cannot refresh token",
-                        request=token_response.request,
-                        response=token_response,
-                    )
+            # NOTE:3rd-party-bug::HTTPX requires_response_body is set on
+            # the class, so I should be able to use token_response.text
+            # instead
+            token_response_data = (await token_response.aread()).decode("utf-8")
+            if not token_response_data:
+                raise HTTPStatusError(
+                    message="no token in response",
+                    request=token_response.request,
+                    response=token_response,
+                )
 
-                # NOTE:3rd-party-bug::HTTPX requires_response_body is set on
-                # the class, so I should be able to use token_response.text
-                # instead
-                token_response_data = (await token_response.aread()).decode("utf-8")
-                if not token_response_data:
-                    raise HTTPStatusError(
-                        message="no token in response",
-                        request=token_response.request,
-                        response=token_response,
-                    )
-
-                try:
-                    access_token = AccessToken.parse_raw(token_response_data)  # type: ignore
-                except pydantic.ValidationError as error:
-                    raise HTTPStatusError(
-                        message="invalid token in response",
-                        request=token_response.request,
-                        response=token_response,
-                    ) from error
-                self.token = access_token.access_token
+            try:
+                access_token = AccessToken.parse_raw(token_response_data)  # type: ignore
+            except pydantic.ValidationError as error:
+                raise HTTPStatusError(
+                    message="invalid token in response",
+                    request=token_response.request,
+                    response=token_response,
+                ) from error
+            self.token = access_token.access_token
 
         # NOTE:TYPES httpx.Request lists the type of headers as requiring a
         # Dict, when it'd be fine with a Mapping
