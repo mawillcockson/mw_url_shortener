6c983acf2f75e95a3f061a42d25a3e70962055f6
Matthew Willcockson <matthew@willcockson.family>
2022-01-15 15:43:10 -0600

    fix(interfaces/remote): correct get_by_id and update
    most of the processing is done by the api, no need to do it locally

--- mw_url_shortener/interfaces/remote/base.py
+++ mw_url_shortener/interfaces/remote/base.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import Dict, Generic, List, Optional, Type, TypeVar, Union
 
 from httpx import AsyncClient, HTTPStatusError, Request, Response
+from pydantic import parse_raw_as
 
 from mw_url_shortener.interfaces.base import (
     CreateSchemaType,
@@ -35,12 +36,16 @@ class RemoteInterfaceBase(
         self, opened_resource: AsyncClient, /, *, id: int
     ) -> Optional[ObjectSchemaType]:
         params = {"id": id}
-        async with opened_resource as client:
-            response = await client.get(f"/v0/{self.endpoint}/", params=params)
-        schema_json = response.text
-        if not schema_json:
-            return None
-        return self.schema.parse_raw(schema_json)
+        response = await opened_resource.get(f"/v0/{self.endpoint}/", params=params)
+
+        schema_type = self.schema
+        object_schemas = parse_raw_as(List[schema_type], response.text)  # type: ignore
+
+        assert len(object_schemas) == 1, f"got more than one {self.endpoint} with the same id: {object_schemas}"
+
+        object_schema = object_schemas[0]
+
+        return object_schema
 
     async def create(
         self,
@@ -66,17 +71,21 @@ class RemoteInterfaceBase(
         current_object_schema: ObjectSchemaType,
         update_object_schema: UpdateSchemaType,
     ) -> Optional[ObjectSchemaType]:
-        update_data = update_object_schema.dict(exclude_unset=True)
-        updated_object = current_object_schema.copy(exclude={"id"}, update=update_data)
-        new_object_json = updated_object.json(exclude_none=True)
-
-        if not new_object_json:
-            return current_object_schema
-
-        async with opened_resource as client:
-            response = await client.put(
-                f"/v0/{self.endpoint}/", content=new_object_json.encode("utf-8")
-            )
+        current_schema_json = current_object_schema.json()
+        update_schema_json = update_object_schema.json()
+        # NOTE:FUTURE partial serialization would allow for serializing
+        # pydantic.BaseModels into an object that json.dumps can encode, and
+        # can thus be included in other arbitrary data, like a list
+        # https://github.com/samuelcolvin/pydantic/issues/951
+        update_body = (
+            "{"
+            f'"current_object_schema":{current_schema_json},'
+            f'"update_object_schema":{update_schema_json}'
+            "}"
+        )
+        response = await opened_resource.put(
+            f"/v0/{self.endpoint}/", content=update_body.encode("utf-8")
+        )
         schema_json = response.text
         return self.schema.parse_raw(schema_json)
 
