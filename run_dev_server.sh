d905af3987243fcb4ffc569ece606c32c0140ad9
Matthew Willcockson <matthew@willcockson.family>
2022-01-22 05:51:14 -0600

    test: note root_path
    can't use it without a reverse proxy

--- run_dev_server.sh
+++ run_dev_server.sh
@@ -2,6 +2,7 @@
 set -eu
 export MW_URL_SHORTENER__DATABASE_PATH="$(pwd)/temp.sqlitedb"
 export MW_URL_SHORTENER__JWT_SECRET_KEY="not very secret"
+# export MW_URL_SHORTENER__ROOT_PATH="root_path"
 export MW_URL_SHORTENER__API_PREFIX="api_prefix"
 
 mw-redir-server debug
