595842bde6f1ccf21d7ae47367b48499c4955fde
Matthew Willcockson <matthew@willcockson.family>
2022-01-22 06:04:39 -0600

    refactor(di/remote): satisfy mypy

--- mw_url_shortener/dependency_injection/remote_interface.py
+++ mw_url_shortener/dependency_injection/remote_interface.py
@@ -6,6 +6,6 @@ from .interfaces import get_resource
 def get_async_client() -> AsyncClient:
     resource = get_resource()
     assert isinstance(
-        resource, AsyncClient
+        resource, AsyncClient  # type: ignore
     ), f"expected resource to be AsyncClient, got {type(resource)} '{resource}'"
     return resource
