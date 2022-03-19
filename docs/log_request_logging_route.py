2cb014d6e5159822ca1ecf47f44f39635356c3f9
Matthew Willcockson <matthew@willcockson.family>
2022-03-19 13:32:51 -0500

    docs(log): cover error cases in previous example

---
+++ docs/log_request_logging_route.py
@@ -0,0 +1,110 @@
+# mypy: allow_any_expr
+"""
+I want to be able to log the request body, especially for non-matching
+requests.
+
+This is an adaptation of the recipe from the FastAPI docs:
+https://fastapi.tiangolo.com/tutorial/handling-errors/#re-use-fastapis-exception-handlers
+
+Since this only logs errors, this would either have to be combined with a
+logging route class that's called when nothing about the request is invalid, or
+each endpoint would have to do the logging.
+
+Here, the first option is demonstrated.
+"""
+import asyncio
+import inspect
+from pprint import pformat, pprint
+from typing import TYPE_CHECKING
+
+import httpx
+from fastapi import Body, FastAPI, Request, Response
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from fastapi.routing import APIRoute
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+if TYPE_CHECKING:
+    from typing import Callable, Coroutine
+
+NUMBER_OF_LOGS: int = 0
+
+
+async def root() -> str:
+    return "hello"
+
+
+class LoggingAPIRoute(APIRoute):
+    def get_route_handler(
+        self,
+    ) -> "Callable[[Request], Coroutine[None, None, Response]]":
+        original_route_handler = super().get_route_handler()
+
+        async def log_request_handler(request: Request) -> Response:
+            # contrary to the recipe in FastAPI,
+            # starlette.requests.Request.body() already caches the request body
+            body = await request.body()
+            global NUMBER_OF_LOGS
+            NUMBER_OF_LOGS += 1
+            return await original_route_handler(request)
+
+        return log_request_handler
+
+
+app = FastAPI()
+app.router.route_class = LoggingAPIRoute
+app.get("/")(root)
+
+
+async def logging_http_exception_handler(
+    request: Request, exc: StarletteHTTPException
+) -> Response:
+    global NUMBER_OF_LOGS
+    NUMBER_OF_LOGS += 1
+    print("HTTP EXCEPTION")
+    pprint(request.scope)
+    return await http_exception_handler(request, exc)
+
+
+async def custom_request_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> Response:
+    global NUMBER_OF_LOGS
+    NUMBER_OF_LOGS += 1
+    print("REQUEST VALIDATION ERROR")
+    pprint(request.scope)
+    return await request_validation_exception_handler(request, exc)
+
+
+app.exception_handler(StarletteHTTPException)(logging_http_exception_handler)
+app.exception_handler(RequestValidationError)(
+    custom_request_validation_exception_handler
+)
+
+
+async def main() -> None:
+    async with httpx.AsyncClient(app=app, base_url="http://does-not-matter") as client:
+        response = await client.get("/")  # 1
+        with open(__file__, "rb") as file:
+            response = await client.post("/", files={"upload-file": file})  # 2
+        non_http_verb_request = client.build_request("NONHTTP", client.base_url)
+        response = await client.send(non_http_verb_request)  # 3
+        response = await client.head("/")  # 4
+        response = await client.post("/")  # 5
+        global NUMBER_OF_LOGS
+        print(f"NUMBER_OF_LOGS: {NUMBER_OF_LOGS} == 5 -> {NUMBER_OF_LOGS == 5}")
+
+
+if __name__ == "__main__":
+    # alternatively, run
+    # hypercorn \
+    #     --access-logfile=- \
+    #     --error-logfile=- \
+    #     --debug \
+    #     request_and_response:app
+    #
+    # then make a request with a browser or cli client
+    asyncio.run(main())
